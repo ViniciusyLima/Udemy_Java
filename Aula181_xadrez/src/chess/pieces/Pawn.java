6e821c5d2518b5f7f311c6059c942b0ac1d72bfa
Vinicius Lima <viniuciusyl@hotmail.com>
2023-12-01 19:48:51 -0300

    Bishop

--- Aula181_xadrez/src/chess/pieces/Pawn.java
+++ Aula181_xadrez/src/chess/pieces/Pawn.java
@@ -10,6 +10,11 @@ public class Pawn extends ChessPiece {
 	public Pawn(Board board, Color color) {
 		super(board, color);
 	}
+	
+	@Override
+	public String toString() {
+		return "P";
+	}
 
 	@Override
 	public boolean[][] possibleMoves() {
@@ -56,10 +61,5 @@ public class Pawn extends ChessPiece {
 			}
 		}
 		return mat;
-	}
-	
-	@Override
-	public String toString() {
-		return "P";
 	}
 }
