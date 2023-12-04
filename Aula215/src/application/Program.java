fd10bf3bfc49b19dea55a7fdce6aa5ee80304487
Vinicius Lima <viniuciusyl@hotmail.com>
2023-12-04 15:19:07 -0300

    Aula215 - Try With Resources

--- Aula215/src/application/Program.java
+++ Aula215/src/application/Program.java
@@ -9,13 +9,9 @@ public class Program {
 	public static void main(String[] args) {
 
 		String path = "c:\\temp\\in.txt";
-		FileReader fr = null;
-		BufferedReader br = null;
+
+		try (BufferedReader br = new BufferedReader(new FileReader(path))){
 		
-		try {
-			fr = new FileReader(path);
-			br = new BufferedReader (fr);
-			
 			String line = br.readLine();
 			
 			 while (line != null) {
@@ -25,19 +21,6 @@ public class Program {
 		}
 		catch (IOException e) {
 			System.out.println("Error: " + e.getMessage());
-		}
-		finally {
-			try {
-				if (br != null) {
-					br.close();
-				}
-				if (fr != null) {
-					fr.close();
-				}
-			}
-			catch (IOException e) {
-				e.printStackTrace();
-			}
 		}
 	}
 }
