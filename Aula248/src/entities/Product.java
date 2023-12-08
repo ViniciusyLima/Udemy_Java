d00b69b442ab06d3bc1eb7ef967243268b4f89fc
Vinicius Lima <viniuciusyl@hotmail.com>
2023-12-08 13:59:30 -0300

    Map2

--- Aula248/src/entities/Product.java
+++ Aula248/src/entities/Product.java
@@ -1,5 +1,7 @@
 package entities;
 
+import java.util.Objects;
+
 public class Product {
 
 	private String name;
@@ -24,5 +26,22 @@ public class Product {
 
 	public void setPrice(Double price) {
 		this.price = price;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(name, price);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Product other = (Product) obj;
+		return Objects.equals(name, other.name) && Objects.equals(price, other.price);
 	}
 }
