49af8232c4bab26e84ab778279d4bb6a0e6072a0
unknown <viniuciusyl@hotmail.com>
2023-12-11 22:36:29 -0300

    Implementação da interface

---
+++ Aula255/src/Util/ProductPredicate.java
@@ -0,0 +1,14 @@
+package Util;
+
+import java.util.function.Predicate;
+
+import entities.Product;
+
+public class ProductPredicate implements Predicate<Product>{
+
+	@Override
+	public boolean test(Product p) {
+		return p.getPrice() >= 100.0;
+	}
+
+}
