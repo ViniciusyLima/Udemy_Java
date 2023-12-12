a3a56be1185ad028fd617e644dbbbc4136ed732a
unknown <viniuciusyl@hotmail.com>
2023-12-12 14:21:30 -0300

    Stream reduce, filters, collect

--- Aula259/src/applications/Program.java
+++ Aula259/src/applications/Program.java
@@ -20,6 +20,6 @@ public class Program {
 		System.out.println(Arrays.toString(st3.limit(10).toArray()));
 		
 		Stream<Long> st4 = Stream.iterate(new Long[] {0L, 1L}, p -> new Long [] {p[1], p[0]+p[1]}).map(p -> p[0]);
-		System.out.println(Arrays.toString(st4.limit(30).toArray()));
+		System.out.println(Arrays.toString(st4.limit(10).toArray()));
 	}
 }
