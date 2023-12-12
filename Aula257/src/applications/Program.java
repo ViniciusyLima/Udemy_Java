5eef34f5b509b4ab2e2a82e76fd947fcff20b2fd
unknown <viniuciusyl@hotmail.com>
2023-12-11 22:55:29 -0300

    Expressão lambda 2° Exemplo

--- Aula257/src/applications/Program.java
+++ Aula257/src/applications/Program.java
@@ -2,6 +2,8 @@ package applications;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
+import java.util.stream.Collectors;
 
 import entities.Product;
 
@@ -9,6 +11,7 @@ public class Program {
 
 	public static void main(String[] args) {
 		
+		Locale.setDefault(Locale.US);
 		List<Product> list = new ArrayList<>();
 		
 		list.add(new Product("TV", 900.00));
@@ -16,10 +19,8 @@ public class Program {
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));
 		
-		double factor = 1.1;
+		List<String> names = list.stream().map(p -> p.getName().toUpperCase()).collect(Collectors.toList());
 		
-		list.forEach(p -> p.setPrice(p.getPrice() * factor));
-		
-		list.forEach(System.out::println);
+		names.forEach(System.out::println);
 	}
 }
--- Aula257/src/util/UpperCaseName.java
+++ Aula257/src/util/UpperCaseName.java
@@ -1,5 +1,14 @@
 package util;
 
-public class UpperCaseName {
+import java.util.function.Function;
+
+import entities.Product;
+
+public class UpperCaseName implements Function<Product, String>{
+
+	@Override
+	public String apply(Product p) {
+		return p.getName().toUpperCase();
+	}
 
 }
