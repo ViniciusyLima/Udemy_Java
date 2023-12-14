10cdb9b5c83e82b180bc35cc81cf7047149a2cb6
unknown <viniuciusyl@hotmail.com>
2023-12-14 09:38:42 -0300

    Deletando dados do BD

--- Aula273-JDBC/src/application/Program.java
+++ Aula273-JDBC/src/application/Program.java
@@ -5,6 +5,7 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 import db.DB;
+import db.DbIntegrityException;
 
 public class Program {
 
@@ -16,19 +17,17 @@ public class Program {
 		try {
 			conn = DB.getConnection();
 			st = conn.prepareStatement(
-					"UPDATE seller " 
-					+ "SET BaseSalary = BaseSalary + ? " 
+					"DELETE FROM department "
 					+ "WHERE "
-					+ "(DepartmentId = ?)");
+					+ "Id = ?");
 			
-			st.setDouble(1, 200.0);
-			st.setInt(2, 2);
-			
+			st.setInt(1, 2);
+					
 			int rowsAffected = st.executeUpdate();
 			System.out.println("Done! Rows affected: "+rowsAffected);
 		}
 		catch (SQLException e) {
-			e.printStackTrace();
+			throw new DbIntegrityException(e.getMessage());
 		}
 		finally {
 			DB.closeStatement(st);
