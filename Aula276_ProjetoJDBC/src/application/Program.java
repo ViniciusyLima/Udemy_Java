b4f7d4141b54a14bee965e01d1d682c7c054ab66
unknown <viniuciusyl@hotmail.com>
2023-12-14 10:12:02 -0300

    Class Department

--- Aula276_ProjetoJDBC/src/application/Program.java
+++ Aula276_ProjetoJDBC/src/application/Program.java
@@ -1,17 +1,13 @@
 package application;
 
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.sql.Statement;
-
-import db.DB;
-import db.DbException;
+import model.entities.Department;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		
+		Department obj = new Department(1, "Books");
+		System.out.println(obj);
 		
 	}
 }
